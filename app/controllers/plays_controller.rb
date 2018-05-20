d53abb3572ee722749a9fe21456a25a6687257e4
omarahmed103 <omariahmed103@gmail.com>
2018-05-20 18:32:04 -0400

    Added simple form gem and bootstrap gem and created partial view for plays

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -2,4 +2,25 @@ class PlaysController < ApplicationController
 
     def index
     end
+
+    def new
+        @play = Play.new
+    end
+
+    def create
+        @play = Play.new(play_params)
+    end
+
+    def edit
+    end
+
+    def destroy
+    end
+
+    private
+
+    def play_params
+        prams.require(:play).permit(:title, :description, :director)
+    end
+
 end
